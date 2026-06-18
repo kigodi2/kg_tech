3f024b752980daf6e920fcffd2eed2eb6a8fd549
Aggrey Kigodi <aggreykigodi@gmail.com>
2026-06-18 19:39:12 +0300

    migration: force InnoDB storage engine for school_result_correction_batches table

--- database/migrations/2026_06_18_100000_create_school_result_correction_batches_table.php
+++ database/migrations/2026_06_18_100000_create_school_result_correction_batches_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::dropIfExists('school_result_correction_batches');
 
         Schema::create('school_result_correction_batches', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->unsignedSmallInteger('exam_year');
             $table->string('exam_type', 20)->default('psle');
